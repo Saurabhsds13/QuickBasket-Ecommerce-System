40f49dfd5da29297886ddbe2f56805b102bc7fb6
Saurabh Sonawane <saurabhsds13@gmail.com>
2025-05-26 16:02:52 +0530

    initial commit for folder structure

---
+++ dmart-server/src/main/java/com/dmart/clone/DmartServerApplication.java
@@ -0,0 +1,13 @@
+package com.dmart.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmartServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmartServerApplication.class, args);
+	}
+
+}
---
+++ dmart-server/src/test/java/com/dmart/clone/DmartServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmart.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmartServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
